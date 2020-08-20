d4722144f39e71d08b9a8a79f1bfe4005d573160
d wu <dwu@ds-MacBook-Pro.local>
2020-08-19 22:12:44 -0500

    adding initial chat flow

--- fbmsg-clone/src/App.js
+++ fbmsg-clone/src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setinput] = useState("");
+  const [messages, setmessages] = useState([]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setmessages([...messages, input]);
+    setinput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form className="messageInput">
+        <input
+          type="text"
+          className="input"
+          value={input}
+          onChange={(e) => setinput(e.target.value)}
+        ></input>
+        <button
+          className="button"
+          type="submit"
+          onClick={(e) => sendMessage(e)}
         >
-          Learn React
-        </a>
-      </header>
+          SEND
+        </button>
+      </form>
+
+      <div className="messge">
+        {messages.map((msg) => (
+          <p>{msg}</p>
+        ))}
+      </div>
     </div>
   );
 }
